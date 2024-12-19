a73e2aa65ca3ee231a52a189cb0e915591ac4f3e
bhawana-web <bhawnav161102@gmail.com>
2024-12-19 23:11:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function getFormvalue() {
-    //Write your code here
-
+function getFormvalue(event) {
+    // Prevent form submission to avoid page reload
+    event.preventDefault();
+    
+    // Get the values from the input fields
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    
+    // Concatenate the names and display them in an alert
+    alert(firstName + ' ' + lastName);
 }
