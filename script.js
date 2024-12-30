86718f7765ad442afb548c5c031ebc6a8b52cde3
bhawana-web <bhawnav161102@gmail.com>
2024-12-30 21:17:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
 // This function will be called when the form is submitted
-function getFormvalue() {
-  // Get the values of First Name and Last Name from the form
-  const firstName = document.getElementById("firstName").value;
-  const lastName = document.getElementById("lastName").value;
-  
-  // Combine First Name and Last Name and display them in an alert
-  alert(firstName + " " + lastName);
-  
-  // Return false to prevent the form from actually submitting and refreshing the page
-  return false;
-}
